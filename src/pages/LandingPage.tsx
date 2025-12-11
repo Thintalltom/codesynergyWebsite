b967ea0db9b75d8ef0cc0c20362c6280d12466c4
Tomide Adeyanju <tadeyanju@infinion.co>
2025-12-11 12:37:12 +0100

    added all changes

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { Hero } from '../components/Hero';
 import { CoursesSection } from '../components/CoursesSection';
 import { ComingSoon } from '../components/ComingSoon';
